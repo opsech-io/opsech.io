fca4ad828e78bf42e3b8cc4274127e7d3db8c1dd
Michael <xenithorb@users.noreply.github.com>
2015-11-02 17:44:17 -0500

    new_markdown: changed interpreter path (see full message)                 PELICAN_DIR relative path based on script loc                 Creating new article notification                 to_filename() added as function because of Title OR Slug condition                 logic added to reject article generation on missing Title                 logic improved (diff lines 118-131) for opening article by VISUAL and EDITOR choice                     - preferential on kate and vim currently

--- scripts/new_markdown.sh
+++ scripts/new_markdown.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # A script to start a new markdown post for pelican
 #
 # Author: Michael Goodwin
-set -eux
-PELICAN_DIR=".."
-CONTENT_DIR="$( awk -F"[ =\'\"]" '$1 == "PATH" {print $5}' < "${PELICAN_DIR}/pelicanconf.py" )"
+set -eu
+PELICAN_DIR="$(dirname $0)/.."
+CONTENT_DIR="$( awk -F"[ ='\"]" '$1 == "PATH" {print $5}' < "${PELICAN_DIR}/pelicanconf.py" )"
+echo "Creating new article: "
 ARTICLES_DIR="articles"
 YEAR="$(date +'%Y')"
-CONTENT_LOC="${PELICAN_DIR}/${CONTENT_DIR}/${ARTICLES_DIR}/${YEAR}" 
+CONTENT_LOC="${PELICAN_DIR}/${CONTENT_DIR}/${ARTICLES_DIR}/${YEAR}"
 
 # Header varibles to get from the user
 IN_VARS=(
@@ -19,52 +20,82 @@ IN_VARS=(
 	SUMMARY
 )
 
-# Print an array, each element on a new line  
+# Print an array, each element on a new line
 # This use case allows for two linebreaks after output
 # because we want a space between header and article body.
 print_array_newline() {
 	eval printf \"%s\\n\" \"\$\{"${1}"[@]\}\"
-	echo -en "\n\n" 
+	echo -en "\n\n"
 }
 
-# Iterates through each IN_VARS element, and asks the user via stdin 
-# to input information for each. Some of the confusing logic here 
+# We'll take something like "Title: Foo bar" from the array
+# and turn it into "foo_bar.md"
+to_filename() {
+	awk -F': ' '$1 == "'"$1"'" { gsub("[- ]","_",$2); tolower($2); print $2".md" }' < <( print_array_newline output )
+	return $?
+}
+
+# Iterates through each IN_VARS element, and asks the user via stdin
+# to input information for each. Some of the confusing logic here
 # Is because I am using IN_VARS elements for the prompt text as well
 # but am reformatting it to look like it will in the header.
 # Could have probably avoided that by requring normal case IN_VARS elements
-# (i.e. "Title" instead of "TITLE"), but this way negates the need to worry 
-# about case altogether. 
+# (i.e. "Title" instead of "TITLE"), but this way negates the need to worry
+# about case altogether.
 for i in "${!IN_VARS[@]}"; do
 	declare -a output input
-	PROMPT_VAR="${IN_VARS[i],,}"    	   # To lowercase 
+	PROMPT_VAR="${IN_VARS[i],,}"    	   # To lowercase
 	PROMPT_VAR="${PROMPT_VAR^}"     	   # To first char upper case
-	read -rp "${PROMPT_VAR}: " "input[$i]" 
-	[[ ${input[i]} ]] &&    		   # Skip appending to the array if null
-	output[$i]="${PROMPT_VAR}: ${input[i]}"    # Save the entier header line
+	read -rp "${PROMPT_VAR}: " "input[$i]"
+	
+	if [[ ${input[i]} ]]; then		   # Skip appending to the array if null
+		output[$i]="${PROMPT_VAR}: ${input[i]}"    # Save the entier header line
+	elif [[ $PROMPT_VAR == "Title" && -z ${input[i]} ]]; then
+		{ echo "Error: At minimum, \"Title:\" is required."; exit 1; } >&2
+	fi
 done
-
-# Get today's date for the post 
+# Get today's date for the post
 output+=( "Date: $(date)" )
 
-# We need the title in order to output a file name 
-# Since the above loop is agnostic on what we're inputting 
-# I found it necessary to use awk to find it. 
-TITLE=$( awk -F': ' '$1 == "Title" { gsub(" ","_",$2); print $2 }' < <( print_array_newline output ) )
-FILE="${CONTENT_LOC}/${TITLE,,}.md"
+# We need the title in order to output a file name
+# Since the above loop is agnostic on what we're inputting
+# If user input a Slug:, use that, else Title:
+if ( print_array_newline output | grep -q "Slug: " ); then
+	FILE="${CONTENT_LOC}/$(to_filename Slug)"
+else
+	FILE="${CONTENT_LOC}/$(to_filename Title)"
+fi
 
-if [[ -f ${FILE} ]]; then 
-	read -rp "File \"${FILE}\" exists, overwrite? [y/N]" -N1 overwrite_choice 
+# Logic to handle existing articles, ask the user if they
+# want to overwrite first, then either truncate the old file
+# or save the new input somewhere else as what the name would be + -saved
+# If the file doesn't exist at all, just output it
+if [[ -f ${FILE} ]]; then
+	read -rp "File \"${FILE}\" exists, overwrite? [y/N]" -N1 overwrite_choice
 	case "$overwrite_choice" in
 		[Yy])
 			print_array_newline output > "${FILE}"
 		;;
 		[Nn?])
 			echo "Ok. Keeping old file. Current will be saved as ${FILE}-saved"
-			print_array_newline output >> "${FILE}-saved" 
+			print_array_newline output >> "${FILE}-saved"
 		;;
-	esac 
+	esac
 else
-	print_array_newline output >> "${FILE}" 
+	print_array_newline output >> "${FILE}"
 fi
 
-vim +$ +start "${FILE}"
+set +u
+for i in "$VISUAL" "$EDITOR"; do
+	if [[ $i =~ /vim ]]; then
+		"$i" +$ +start "${FILE}"
+		break
+	elif [[ $i =~ /kate ]]; then
+		"$i" -l $(wc -l "${FILE}")
+		break
+	elif [[ -n $i ]]; then
+		"$i" "${FILE}"
+		break
+	fi
+done
+set -u
