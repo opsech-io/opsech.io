74aabfff5d95886384e6841666e3827f27f8417a
Michael <xenithorb@users.noreply.github.com>
2015-11-02 17:37:57 -0500

    Changed path reference for PELICAN_DIR, but mostly EOL space removals

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,33 +1,33 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Setup script to start development on OpSech.io
 # website. Currently only supports Fedora (DNF)
 #
 # Author: Michael Goodwin
 set -eu
-PELICAN_DIR=".."
+PELICAN_DIR="$(dirname $0)/.."
 
 # dnf package install array
 DNF_INSTALL=(
 	python{,-virtualenv{,wrapper},-devel}
-	libjpeg-turbo-devel 
+	libjpeg-turbo-devel
 	zlib-devel
 	s3cmd	
 )
 
 # pip package install array
-PIP_INSTALL=( 
+PIP_INSTALL=(
 	markdown
-	pillow 
+	pillow
 	beautifulsoup4
 	typogrify
 )	
 
-# Install based pre-virtualenv requirements 
+# Install based pre-virtualenv requirements
 # libjpeg and zlib devel are for python compiling the Pillow library (see below)
 sudo dnf install "${DNF_INSTALL[@]}"
 
-# Setup python virtual environment for working with pelican 
+# Setup python virtual environment for working with pelican
 (
 set +u
 mkdir -p ~/virtualenvs
@@ -37,8 +37,8 @@ pip install "${PIP_INSTALL[@]}"
 )
 
 # We need to pull in the long tree of plugins recursively
-# Since the pelican-plugins git project also links to some 
-# plugins as submodules. 
+# Since the pelican-plugins git project also links to some
+# plugins as submodules.
 (
 cd "${PELICAN_DIR}"
 git submodule update --init --recursive
@@ -48,10 +48,10 @@ git submodule update --init --recursive
 git submodule foreach --recursive 'git checkout master; git pull'
 )
 # Setup s3cmd
-# Go get ACCESS_KEY and SECRET_ACCESS_KEY that 
-# Can R/W to the proper site bucket! 
-# _MUST_ be the same name as the domain if you intend 
+# Go get ACCESS_KEY and SECRET_ACCESS_KEY that
+# Can R/W to the proper site bucket!
+# _MUST_ be the same name as the domain if you intend
 # to serve your content with S3 as the static server!
 if [[ ! -e ~/.s3cfg ]]; then
 	s3cmd --configure
-fi 
+fi
