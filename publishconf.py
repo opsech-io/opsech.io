df3e85e58920408038341c9eecbc576cd8b724a5
Michael <xenithorb@users.noreply.github.com>
2016-01-15 12:25:06 -0500

    Changed piwik URL to evade EasyPrivacy default settings

--- publishconf.py
+++ publishconf.py
@@ -21,7 +21,7 @@ CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 DELETE_OUTPUT_DIRECTORY = True
 DISQUS_SITENAME = "opsech"
 
-PIWIK_URL = '//opsech-io.piwikpro.com/'
+PIWIK_URL = '//opsech-io.piwik.pro/'
 PIWIK_SITE_ID = 1
 
 # Below is for making sure that only "Status: published" articles go live.
